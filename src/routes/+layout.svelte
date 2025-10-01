a68bb919b267a28b1b65bb15d6defb689caa5aa2
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-10-01 11:44:25 -0500

    feat: add mobile screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,14 @@
 	let { children } = $props();
 </script>
 
-<div class="flex flex-col h-screen">
+<div class="flex-col h-screen hidden lg:flex">
     <Navbar/>
     <div class="flex-1">
         {@render children?.()}
     </div>
+</div>
+
+<div class="lg:hidden flex items-center justify-center h-screen">
+    <h1 class="text-3xl font-bold max-w-3/4">Switch to desktop to use Evergreen.</h1>
 </div>
 
